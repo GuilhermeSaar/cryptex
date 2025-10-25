59a096c4db75e4b2b8d38ffd712c8d381280d229
Guilherme <guilhermesaar02@gmail.com>
2025-10-24 21:22:59 -0300

    initial config

---
+++ src/main/java/com/gstech/cryptex/CryptexApplication.java
@@ -0,0 +1,13 @@
+package com.gstech.cryptex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gstech/cryptex/CryptexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gstech.cryptex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
